b4bad5a31daa98488537b3e8650332d410173c00
Diego Cardoso <diegocar448@hotmail.com>
2024-04-02 11:08:07 -0300

    implementacao

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,60 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+class Usuario(val nome: String)
 
-class Usuario
+data class ConteudoEducacional(val nome: String, val duracao: Int = 60)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
-
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
-
+data class Formacao(val nome: String, val nivel: Nivel, val conteudos: List<ConteudoEducacional>) {
+    
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
+    }
+    
+    fun desmatricular(usuario: Usuario) {
+        inscritos.remove(usuario)
+    }
+    
+    fun listarInscritos() {
+        println("Usuários inscritos na formação $nome:")
+        inscritos.forEach { println(it.nome) }
     }
 }
 
+fun printMessageWithPrefix(message: String, prefix: String = "Info"){
+    println("[$prefix] $message")
+}
+
+fun sum(x: Int, y: Int): Int{
+    return x + y
+}
+
+fun multiply(x: Int, y: Int) = x * y
+
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val usuario1 = Usuario("João")
+    val usuario2 = Usuario("Maria")
+    val usuario3 = Usuario("Pedro")
+    
+    val conteudo1 = ConteudoEducacional("Introdução à Programação", 90)
+    val conteudo2 = ConteudoEducacional("Estruturas de Dados", 120)
+    val conteudo3 = ConteudoEducacional("Algoritmos Avançados", 150)
+    
+    val formacao1 = Formacao("Desenvolvimento de Software", Nivel.BASICO, listOf(conteudo1))
+    val formacao2 = Formacao("Ciência da Computação", Nivel.AVANCADO, listOf(conteudo1, conteudo2, conteudo3))
+    
+    formacao1.matricular(usuario1)
+    formacao1.matricular(usuario2)
+    formacao2.matricular(usuario2)
+    formacao2.matricular(usuario3)
+    
+    formacao1.listarInscritos()
+    formacao2.listarInscritos()
+    
+    formacao2.desmatricular(usuario2)
+    formacao2.listarInscritos()
+    
+    // Chamar funções adicionais aqui, se necessário
+    printMessageWithPrefix("Hello", "Log")
 }
